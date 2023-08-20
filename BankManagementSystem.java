31c39f923f4aa3b64e99f9e39deffaea688e2208
Ronita Adhikari <ronitaadhikari555@gmail.com>
2023-08-20 21:15:13 +0530

    Added a getPreviousTransaction method

--- BankManagementSystem.java
+++ BankManagementSystem.java
@@ -9,8 +9,8 @@ class BankAccount
             balance=balance+amount;
             previousTransaction=amount;
         }
-        System.out.println("Deposited: "+amount);
-        System.out.println("Current balance: "+balance);
+        // System.out.println("Deposited: "+amount);
+        // System.out.println("Current balance: "+balance);
     }
     void withdraw(int amount)
     {
@@ -19,10 +19,24 @@ class BankAccount
             balance=balance-amount;
             previousTransaction= -amount;
         }
-        System.out.println("Withdrawn: "+amount);
-        System.out.println("current balance: "+balance);
+        // System.out.println("Withdrawn: "+amount);
+        // System.out.println("current balance: "+balance);
     }
-    
+    void getPreviousTransaction()
+    {
+        if(previousTransaction>0)
+        {
+            System.out.println("Deposited: "+previousTransaction);
+        }
+        else if(previousTransaction<0)
+        {
+            System.out.println("withdrawn: "+Math.abs(previousTransaction));
+        }
+        else
+        {
+            System.out.println("No Transaction occured");
+        }
+    }
 
 }
 
@@ -31,6 +45,7 @@ public class BankManagementSystem {
     public static void main(String[] args) {
         BankAccount ob=new BankAccount();
         ob.deposit(10000);
-        ob.withdraw(5000);
+        ob.withdraw(2000);
+        ob.getPreviousTransaction();
     }
 }
