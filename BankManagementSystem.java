bf965e39dbdb867b590f1cea7ab91360b4c1ac9e
Ronita Adhikari <ronitaadhikari555@gmail.com>
2023-08-20 20:53:24 +0530

    Added a deposit method

--- BankManagementSystem.java
+++ BankManagementSystem.java
@@ -1,11 +1,24 @@
 class BankAccount 
 {
-    
+    int balance=0;
+    int previousTransaction;
+    void deposit(int amount)
+    {
+        if(amount!=0)
+        {
+            balance=balance+amount;
+            previousTransaction=balance;
+        }
+        System.out.println("Deposited: "+amount);
+        System.out.println("Current balance: "+balance);
+    }
+
 }
 
 
 public class BankManagementSystem {
     public static void main(String[] args) {
-        
+        BankAccount ob=new BankAccount();
+        ob.deposit(10000);
     }
 }
