3d54a5835954fcf181f15578bb3fb3aaac1671a8
Rohan Nag <nagrohan579@gmail.com>
2023-08-23 07:58:40 +0530

    Added separate input method to take inputs

--- BankManagementSystem.java
+++ BankManagementSystem.java
@@ -1,110 +1,92 @@
 import java.util.Scanner;
 
-class BankAccount 
-{
-    int balance=0;
-    int previousTransaction;
-    String customerName;
-    int customerId;
-    BankAccount(String cName,int cId)
-    {
-        customerName=cName;
-        customerId=cId;
+class BankAccount {
+    private int balance = 0;
+    private int previousTransaction;
+    private String customerName;
+    private int customerId;
+    private Scanner sc;
+
+    BankAccount(String cName, int cId) {
+        this.sc = new Scanner(System.in);
+        customerName = cName;
+        customerId = cId;
     }
-    void deposit(int amount)
-    {
-        if(amount!=0)
-        {
-            balance=balance+amount;
-            previousTransaction=amount;
-        }
-        // System.out.println("Deposited: "+amount);
-        // System.out.println("Current balance: "+balance);
+
+    int input() {
+        return Integer.parseInt(sc.nextLine());
     }
-    void withdraw(int amount)
-    {
-        if(amount!=0)
-        {
-            balance=balance-amount;
-            previousTransaction= -amount;
+
+    void deposit() {
+        System.out.println("Enter an amount to deposit:");
+        int amount = input();
+        if (amount != 0) {
+            balance = balance + amount;
+            previousTransaction = amount;
         }
-        // System.out.println("Withdrawn: "+amount);
-        // System.out.println("current balance: "+balance);
     }
-    void getPreviousTransaction()
-    {
-        if(previousTransaction>0)
-        {
-            System.out.println("Deposited: "+previousTransaction);
+
+    void withdraw() {
+        System.out.println("Enter an amount to withdraw: ");
+        int amount = input();
+        if (amount != 0) {
+            balance = balance - amount;
+            previousTransaction = -amount;
         }
-        else if(previousTransaction<0)
-        {
-            System.out.println("withdrawn: "+Math.abs(previousTransaction));
-        }
-        else
-        {
+    }
+
+    void getPreviousTransaction() {
+        if (previousTransaction > 0) {
+            System.out.println("Deposited: " + previousTransaction);
+        } else if (previousTransaction < 0) {
+            System.out.println("withdrawn: " + Math.abs(previousTransaction));
+        } else {
             System.out.println("No Transaction occured");
         }
-        
+
     }
-    void showMenu()
-    {
+
+    void showMenu() {
         int option;
-        Scanner sc = new Scanner(System.in);
+
         try {
-            System.out.println("Welcome "+customerName);
-            System.out.println("Your Id is: "+customerId);
+            System.out.println("Welcome " + customerName);
+            System.out.println("Your Id is: " + customerId);
             System.out.println("1. Check Balance");
             System.out.println("2. Deposit");
             System.out.println("3. Withdraw");
             System.out.println("4. Previous Transaction");
             System.out.println("5. Exit");
 
-            do{
+            do {
                 System.out.println("Enter an option: ");
-                option=sc.nextInt();
+                option = input();
 
-                switch(option)
-                {
-                    case 1:
-                        System.out.println("Balance: "+balance);
-                        break;
-                    case 2:
-                        System.out.println("Enter an amount to deposit:");
-                        int amount=sc.nextInt();
-                        deposit(amount);
-                        break;
-                    case 3:
-                        System.out.println("Enter an amount to withdraw: ");
-                        int amount2=sc.nextInt();
-                        withdraw(amount2);
-                        break;
-                    case 4:
-                        getPreviousTransaction();
-                        break;
-                    case 5:
-                        System.out.println("******************");
-                        break;
-                    default:
-                        System.out.println("Invalid position");
-                        break;
+                switch (option) {
+                    case 1 -> System.out.println("Balance: " + balance);
+                    case 2 -> deposit();
+                    case 3 -> withdraw();
+                    case 4 -> getPreviousTransaction();
+                    case 5 -> System.out.println("******************");
+                    default -> System.out.println("Invalid input!");
                 }
-
-
-            }while(option!=5);
-        } 
-        finally{
+            } while (option != 5);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+         finally {
             sc.close();
         }
     }
-    
 
 }
 
-
 public class BankManagementSystem {
     public static void main(String[] args) {
-        BankAccount ob=new BankAccount("Ronita",987);
+        BankAccount ob = new BankAccount("Ronita", 987);
         ob.showMenu();
     }
 }
